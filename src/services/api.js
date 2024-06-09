7ea1d147cf324b1d15915e2fcb3a4677e277a539
Soner <syln.soner@gmail.com>
2024-06-09 16:14:38 +0300

    feat: Set basic authentication headers in loginUser function
    The `loginUser` function in `api.js` now sets the basic authentication headers using the provided credentials. This ensures that the user's login request is authenticated properly.

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-
 const API_BASE_URL = 'http://localhost:8080/api';
 
+let basicAuth = null;
+
+export const setBasicAuth = (username, password) => {
+  basicAuth = 'Basic ' + btoa(username + ':' + password);
+  axios.defaults.headers.common['Authorization'] = basicAuth;
+};
 
 export const getLabels = () => axios.get(`${API_BASE_URL}/labels`);
 export const getLabelById = (id) => axios.get(`${API_BASE_URL}/labels/${id}`);
@@ -20,4 +25,11 @@ export const deleteTask = (id) => axios.delete(`${API_BASE_URL}/tasks/${id}`);
 export const addLabelToTask = (taskId, labelId) => axios.post(`${API_BASE_URL}/tasks/${taskId}/labels/${labelId}`);
 
 export const registerUser = (user) => axios.post(`${API_BASE_URL}/register`, user);
-export const loginUser = (credentials) => axios.post(`${API_BASE_URL}/login`, credentials);
+
+export const loginUser = (credentials) => {
+  return axios.post(`${API_BASE_URL}/login`, credentials)
+    .then(response => {
+      setBasicAuth(credentials.username, credentials.password);
+      return response;
+    });
+};
